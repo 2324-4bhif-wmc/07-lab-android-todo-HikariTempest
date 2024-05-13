94301968f23821b8851eed4a956ed9f1c8ff7363
HikariTempest <arwed@gmx.at>
2024-05-13 12:34:21 +0200

    feat: added the util classes and finally used the configuration by logging the json.placeholder.baseurl config

--- todo/app/src/main/java/at/htl/todo/MainActivity.java
+++ todo/app/src/main/java/at/htl/todo/MainActivity.java
@@ -1,13 +1,17 @@
 package at.htl.todo;
 
 import android.os.Bundle;
+import android.util.Log;
+
 import androidx.activity.ComponentActivity;
 import javax.inject.Inject;
 import at.htl.todo.ui.layout.MainView;
+import at.htl.todo.util.Config;
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 public class MainActivity extends ComponentActivity {
+    static final String TAG  = MainActivity.class.getSimpleName();
 
     @Inject
     MainView mainView;
@@ -15,6 +19,9 @@ public class MainActivity extends ComponentActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Config.load(this);
+        var base_url = Config.getProperty("json.placeholder.baseurl");
+        Log.i(TAG, "onCreate: " + base_url);
         mainView.buildContent(this);
     }
 }
